7e285115e9622f3f1a9c06ebc6cad1de55bbb696
Tudor Caseru <theoriginaltudor@gmail.com>
2025-10-23 14:08:53 +0200

    add comments store and operations

---
+++ InteractiveComments/src/stores/comments.ts
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { createComment, deleteComment, getComments, type Comment } from '../../utils/apiCall'
+
+export const useCommentsStore = defineStore('comments', () => {
+  const comments = ref<Comment[] | null>(null)
+  const error = ref<string | undefined>()
+
+  const add = async (comment: Comment) => {
+    const { data, error: createError } = await createComment(comment)
+    if (createError) {
+      error.value = 'Create error:' + createError.message
+      return
+    }
+    if (data) {
+      if (comments.value !== null) comments.value.push(data)
+      else comments.value = [data]
+    }
+  }
+
+  const remove = async (id: number) => {
+    const { error: deleteError } = await deleteComment(id)
+    if (deleteError) {
+      error.value = 'Delete error:' + deleteError.message
+      return
+    }
+  }
+
+  const get = async () => {
+    const { data, error: getError } = await getComments()
+    if (getError) {
+      error.value = 'Get error:' + getError.message
+      return
+    }
+    if (data) {
+      comments.value = data
+    }
+  }
+
+  return { error, comments, add, remove, get }
+})
