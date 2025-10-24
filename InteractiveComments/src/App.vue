cedec6377f3d7f9e4386a703f60a66dd22491846
Tudor Caseru <theoriginaltudor@gmail.com>
2025-10-24 11:26:54 +0200

    test spacing

--- InteractiveComments/src/App.vue
+++ InteractiveComments/src/App.vue
@@ -3,7 +3,7 @@ import Typography from './components/Typography.vue'
 </script>
 
 <template>
-  <main class="p-10">
+  <main class="p-10 flex flex-col gap-200">
     <Typography as="h1" :preset="1">Hello here</Typography>
     <Typography as="h2" :preset="2">Hello here</Typography>
     <Typography as="h3" :preset="3">Hello here</Typography>
