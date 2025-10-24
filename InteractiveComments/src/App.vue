faf3f2fe10a334a1bd0f37dcf2efbfbc3bd12c12
Tudor Caseru <theoriginaltudor@gmail.com>
2025-10-24 10:01:16 +0200

    clean up and adding new colors

--- InteractiveComments/src/App.vue
+++ InteractiveComments/src/App.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <h1>Hi</h1>
   </main>
 </template>
