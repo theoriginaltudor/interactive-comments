efdbce86dc99bc260a71ec529ed1acf35dcdad92
Tudor Caseru <theoriginaltudor@gmail.com>
2025-10-24 13:57:39 +0200

    extend mainbutton for reusability

--- InteractiveComments/src/components/MainButton.vue
+++ InteractiveComments/src/components/MainButton.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 import { cn } from '../../utils/styleMerge'
 import Typography from './Typography.vue'
-const props = defineProps<{ disabled?: boolean }>()
+const props = defineProps<{ disabled?: boolean; class?: string }>()
 </script>
 
 <template>
   <button
     :class="
-      cn('text-white bg-purple-600 cursor-pointer rounded-lg px-[27px] py-3 uppercase', {
-        'bg-purple-200 cursor-default': disabled,
-      })
+      cn(
+        'text-white bg-purple-600 cursor-pointer rounded-lg px-[27px] py-3 uppercase',
+        {
+          'bg-purple-200 cursor-default': disabled,
+        },
+        props.class,
+      )
     "
   >
     <Typography :preset="2">
